68dcfdaacc0625efabd5f568613e1d54d2647668
Alon Ben Haim <alon.b.h.672@gmail.com>
2019-03-23 21:27:50 +0200

    Removed maintenance

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use((req, res, next) => {
     var now = new Date().toString();
